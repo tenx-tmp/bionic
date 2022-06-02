ef7e0bb8dc3aa07e034e7d074547e1d69f1a6c54
Christopher Ferris <cferris@google.com>
2022-06-02 15:20:23 -0700

    Verify that allocated memory is always zeroed.
    Test: New unit tests pass on scudo.
    Test: New unit tests fail on original jemalloc.
    Test: New unit tests pass on modified jemalloc.
    
    Change-Id: Ia02f151aa37b5c39e986f0cdda42630081bac71d
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
    Signed-off-by: Anush02198 <anush.4376@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: KUIpru <rakeshtiwari349@gmail.com>

--- libc/kernel/uapi/linux/netfilter_ipv4/ipt_ecn.h
+++ libc/kernel/uapi/linux/netfilter_ipv4/ipt_ecn.h
@@ -16,15 +16,22 @@
  ***
  ****************************************************************************
  ****************************************************************************/
-#ifndef _IPT_ECN_H
-#define _IPT_ECN_H
-#include <linux/netfilter/xt_ecn.h>
-#define ipt_ecn_info xt_ecn_info
-enum {
-  IPT_ECN_IP_MASK = XT_ECN_IP_MASK,
-  IPT_ECN_OP_MATCH_IP = XT_ECN_OP_MATCH_IP,
-  IPT_ECN_OP_MATCH_ECE = XT_ECN_OP_MATCH_ECE,
-  IPT_ECN_OP_MATCH_CWR = XT_ECN_OP_MATCH_CWR,
-  IPT_ECN_OP_MATCH_MASK = XT_ECN_OP_MATCH_MASK,
+#ifndef _IPT_ECN_TARGET_H
+#define _IPT_ECN_TARGET_H
+#include <linux/types.h>
+#include <linux/netfilter/xt_DSCP.h>
+#define IPT_ECN_IP_MASK (~XT_DSCP_MASK)
+#define IPT_ECN_OP_SET_IP 0x01
+#define IPT_ECN_OP_SET_ECE 0x10
+#define IPT_ECN_OP_SET_CWR 0x20
+#define IPT_ECN_OP_MASK 0xce
+struct ipt_ECN_info {
+  __u8 operation;
+  __u8 ip_ect;
+  union {
+    struct {
+      __u8 ece : 1, cwr : 1;
+    } tcp;
+  } proto;
 };
 #endif
